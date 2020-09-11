417f6dd28772b37b2595c7faca71085917a28b0b
jaxwang <jaxwang98@gmail.com>
2020-09-11 17:43:37 +0800

    代码修正

--- ZJDemoProduct_OC/ZJDemoVideos/Controller/ZJVideoPlayer.m
+++ ZJDemoProduct_OC/ZJDemoVideos/Controller/ZJVideoPlayer.m
@@ -8,12 +8,14 @@
 
 #import "ZJVideoPlayer.h"
 #import <AVFoundation/AVFoundation.h>
+#import <AVKit/AVKit.h>
 
 @interface ZJVideoPlayer ()
 
 @property (nonatomic, strong) AVPlayerItem *videoItem;//播放器item
 @property (nonatomic, strong) AVPlayer *avPlayer;//播放器layer
 @property (nonatomic, strong) AVPlayerLayer *playerLayer;//播放器layer
+
 
 @end
 
@@ -36,35 +38,46 @@
     NSURL *viedoURL = [NSURL URLWithString:videoUrl];
     AVAsset *asset = [AVAsset assetWithURL:viedoURL];
     self.videoItem = [AVPlayerItem playerItemWithAsset:asset];
+    
+    //监视视频资源状态
     [self.videoItem addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
+    
+    //监视视频缓存状态
     [self.videoItem addObserver:self forKeyPath:@"loadedTimeRanges" options:NSKeyValueObservingOptionNew context:nil];
+    
+    //需要在状态变化后获取时间
     CMTime duration = self.videoItem.duration;
-    CGFloat videoDuration = CMTimeGetSeconds(duration);
+    __unused CGFloat videoDuration = CMTimeGetSeconds(duration);
 
     //controller
     self.avPlayer = [AVPlayer playerWithPlayerItem:self.videoItem];
+    
+    //监视播放器播放进度
     [self.avPlayer addPeriodicTimeObserverForInterval:CMTimeMake(1, 1) queue:dispatch_get_main_queue() usingBlock:^(CMTime time) {
         NSLog(@"播放进度:%@", @(CMTimeGetSeconds(time)));
     }];
+    
     //view
     self.playerLayer = [AVPlayerLayer playerLayerWithPlayer:self.avPlayer];
     self.playerLayer.frame = attachView.bounds;
-
-
     [attachView.layer addSublayer:self.playerLayer];
 
+    //接受播放完成的通知
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_handlePlayEnd) name:AVPlayerItemDidPlayToEndTimeNotification object:nil];
 }
 
 - (void)_stopPlay {
-//        移除播放器
-    [self.playerLayer removeFromSuperlayer];
+    //移除监听
     [self.videoItem removeObserver:self forKeyPath:@"status"];
     [self.videoItem removeObserver:self forKeyPath:@"loadedTimeRanges"];
     [[NSNotificationCenter defaultCenter] removeObserver:self];
-
+    
+    //移除播放器
+    [self.playerLayer removeFromSuperlayer];
     self.videoItem = nil;
     self.avPlayer = nil;
+
+
 }
 
 - (void)_handlePlayEnd {
